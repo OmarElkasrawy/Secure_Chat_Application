74bba7185524a50a3475c7640f5a59a9fe58b0c0
OmarElkasrawy <OA2101689@tkh.edu.eg>
2024-04-21 23:56:38 +0200

    update

--- Secure_Chat_Application/client.cpp
+++ Secure_Chat_Application/client.cpp
@@ -44,6 +44,76 @@ void receive_messages(int sock) { // sock because socket cant be used
 
 
 int main() {
+	// create socket
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) {
+		cerr << "Error creating socket" << endl;
+		return 1;
+	}
 
+	// server address setup
+	sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	inet_pton(AF_INET, SERVER_ADDRESS.c_str(), &server_addr.sin_addr);
+
+	// connection
+	if (connect(sock, (sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+		cerr << "Error connecting to server" << endl;
+		return 1;
+	}
+
+	// auth process
+	string username, password;
+	//flag
+	bool authenticated = false;
+	while (!authenticated) {
+		// request user and pass
+		cout << "Enter username: ";
+		getline(cin, username);
+
+		cout << "Enter password: ";
+		getline(cin, password);
+
+		// clear input buffer test cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		// send user and pass lengths to server
+		int username_length = username.size();
+		send(sock, &username_length, sizeof(username_length), 0);
+
+		send(sock, username.c_str(), username_length, 0);
+
+		int password_length = password.size();
+		send(sock, &password_length, sizeof(password_length), 0);
+		
+		send(sock, password.c_str(), password.size(), 0);
+
+		// recv auth result from server
+		char auth_result[1024];
+		recv(sock, auth_result, sizeof(auth_result), 0);
+		auth_result[sizeof(auth_result) - 1] = '\0'; // null terminate
+		if (string(auth_result) == "OK") {
+			authenticated = true;
+			cout << "Authentication successful!" << endl;
+		}
+		else {
+			cout << "Authentication failed!" << endl;
+		}
+	}
+
+
+
+	//start thread to recv messages from server
+	thread t(receive_messages, sock);
+	t.detach();
+
+	// loop to send msgs to server and recv responses
+	string input;
+	while (getline(cin, input)) {
+		send(sock, input.c_str(), input.size(), 0);
+	}
+
+	//close socket
+	close(sock);
 	return 0;
 }
